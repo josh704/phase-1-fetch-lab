5d7547ef7d842f9f91478c9bd5053d6e46fb2805
josh704 <joshuangetich02@gmail.com>
2024-10-09 17:14:29 +0300

    fetch-lab

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(`renderBooks()`);
+  });
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
